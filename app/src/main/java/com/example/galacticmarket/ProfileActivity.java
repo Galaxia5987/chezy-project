3a0de8181d9fba8dbd59d855c87b779aefe63acb
Adam <adam.vichik@gmail.com>
2021-01-30 18:51:08 +0200

    Add business link

--- app/src/main/java/com/example/galacticmarket/ProfileActivity.java
+++ app/src/main/java/com/example/galacticmarket/ProfileActivity.java
@@ -13,6 +13,7 @@ import org.w3c.dom.Text;
 public class ProfileActivity extends AppCompatActivity {
     private EditText business_name;
     private EditText address;
+    private EditText business_link;
     private EditText business_description;
 
     private Button submit_button;
@@ -22,13 +23,12 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         createView();
-
-
     }
 
     private void createView() {
         business_name = findViewById(R.id.business_name);
         address = findViewById(R.id.address);
+        business_link = findViewById(R.id.business_link);
         business_description = findViewById(R.id.business_description);
         submit_button = findViewById(R.id.submit_button);
 
@@ -38,6 +38,7 @@ public class ProfileActivity extends AppCompatActivity {
             } else {
                 Snackbar.make(view, "Name: " + business_name.getText().toString() +
                                 ", Address: " + address.getText().toString() +
+                                ", Link: " + business_link.getText().toString() +
                                 ", Description: " + business_description.getText().toString(),
                         Snackbar.LENGTH_LONG).show();
             }
